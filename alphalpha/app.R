eb606fbc9f4f007aa643b5cfab5653760d9487c0
Shane Drabing <shane.drabing@gmail.com>
2023-03-07 14:05:45 -0700

    more UAs

--- alphalpha/app.R
+++ alphalpha/app.R
@@ -33,7 +33,11 @@ CGI_YAHOO <- "https://query1.finance.yahoo.com/v7/finance/download/%s?"
 CGI_FRED <- "https://fred.stlouisfed.org/graph/fredgraph.csv?"
 
 # user agents
-AGENTS <- c("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.1")
+AGENTS <- c("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.1",
+            "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36)",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1")
 
 # hints
 TEXT_REFERENCE <- trimws("
@@ -212,8 +216,10 @@ handle_get <- function(
         }
     }
 
-    print(key)
-    print(CACHE[[key]])
+    if (typeof(CACHE[[key]]) == "list") {
+        print(key)
+        print(CACHE[[key]])
+    }
 
     # assign cache to globals
     assign("CACHE", CACHE, .GlobalEnv)
